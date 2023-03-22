d27f4c01bc0ab9ca94323eaae25ae0ce40dfc2cc
luismda <luismigueldutraalves@gmail.com>
2023-03-22 11:03:32 -0300

    Initial commit

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,43 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    size: 'md',
+    children:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem soluta dolore nam iste. Reiciendis optio modi, sed vitae voluptates quisquam animi, illo quam molestias blanditiis quod eaque asperiores expedita accusamus?',
+  },
+  argTypes: {
+    size: {
+      options: [
+        'xxs',
+        'xs',
+        'sm',
+        'md',
+        'lg',
+        'xl',
+        '2xl',
+        '4xl',
+        '5xl',
+        '6xl',
+        '7xl',
+        '8xl',
+        '9xl',
+      ],
+      control: {
+        type: 'select',
+      },
+    },
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
